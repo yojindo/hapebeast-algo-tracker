947c7d7c37360eead2466d83b6565e9a77e1fbe7
Jake Kwak <jakekwak@jakes-mbp.lan>
2021-12-31 21:42:04 -0500

    new project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,114 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import { useState, useEffect } from "react";
+import AddTask from "./components/AddTask";
+import Footer from "./components/Footer";
+import About from "./components/About";
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(true)
+  const [tasks, setTasks] = useState([])
+  
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
+    }
+
+    getTasks()
+  }, [])
+
+  //Fetch tasks
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+
+    return data
+  }
+  
+  //Fetch task
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await res.json()
+
+    return data
+  }
+
+
+  //delete Task
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'DELETE'
+    })
+
+
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  //toggle reminder
+  const toggleReminder = async (id) => {
+
+    const taskToToggle = await fetchTask(id)
+    const updTask = {...taskToToggle,reminder: !taskToToggle.reminder}
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(updTask)
+    })
+
+    const data = await res.json()
+
+    setTasks(
+      tasks.map((task) => 
+        task.id === id ? {...task, reminder: data.reminder} : task
+      )
+    )
+  }
+
+  //Add Task
+const addTask = async (task) => {
+
+  const res = await fetch('http://localhost:5000/tasks', {
+    method: 'POST',
+    headers: {
+    'Content-type': 'application/json',
+    },
+    body: JSON.stringify(task)
+  })
+
+  const data = await res.json()
+
+  setTasks([...tasks, data])
+
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+        <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+        <Routes>
+        <Route path='/' exact element={
+          <>
+          {showAddTask && <AddTask onAdd={addTask}/>}
+          {tasks.length > 0 ? (<Tasks tasks = {tasks} onDelete = {deleteTask} onToggle = {toggleReminder} /> ) : ('Go ape into more Algos!')}
+          </>
+        }/>
+        <Route path='/about' element ={About()} />
+        </Routes>
+        <Footer/>
+      </div>
+
+      <div className ='hapeLogo-container'>
+        <img className = 'hapeLogo' src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/82736b130553681.61829ee7d5466.png"/>
+      </div>
+
+    </Router>
+    
   );
 }
 
-export default App;
+export default App
---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import {Link} from 'react-router-dom'
+
+const About = () => {
+    return (
+        <div>
+        <h4>Fun project for the HAPE coding cave fam. Dedicated to all the HAPE coders out there grinding out algos. WGMI</h4>
+        <Link to='/'>Go Back</Link>            
+        </div>
+    )
+}
+
+export default About
